d54d1579c248476553723b014a036b25381eda00
Izzy-droid <isdm908@gmail.com>
2025-04-08 14:15:33 -0400

    Updating files for repo

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+
+      
+      <div className="cols-arrange">
+        <div className="left-col">
+          <div className="left-arrange">
+          <p>yo</p>
+          </div>
+        </div>
+
+        <div className="middle-col">
+        <div className="middle-inner">
+          <p>vj</p>
+          </div>
+        </div>
+   
+        <div className="right-col">
+          <div className="right-inner">
+          <p>he</p>
+          </div>
+         
+        </div>
+
+
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
